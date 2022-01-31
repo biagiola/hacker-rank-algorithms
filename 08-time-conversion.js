3d9fa353409936dc5f060404ee2d4eaa9fe469f7
David <marcelobiagiola01@gmail.com>
2022-01-30 21:47:05 -0300

    30-01-2147

--- 08-time-conversion.js
+++ 08-time-conversion.js
@@ -1,33 +1,24 @@
 function timeConvertion(s) {
-  let array = [
-    ['13','01'],
-    ['14','02'],
-    ['15','03'],
-    ['16','04'],
-    ['17','05'],
-    ['18','06'],
-    ['19','07'],
-    ['20','08'],
-    ['21','09'],
-    ['22','10'],
-    ['23','11'],
-    ['00','12']
-  ]
-
-  let meridiem = s.slice(s.length-2)
-  let time = []
-
-  //console.log(minuten/*,seconds*/)
-
-  for (let x = 0; x < s.length; x++) {
-    //console.log(array[x][0])
-    if(s[x] == ':') {
-      console.log('holi', x)
-      if(x<3) {
-        time[x] = s[x]
-      }
-    }   
+  let hour =  s.slice(0,2)
+  let minutes = s.slice(3,5)
+  let seconds = s.slice(6,8)
+  let meridiem = s.slice(8,10)
+  let timeMilitaryFormat = ''
+  // 01 2 34 5 67 89
+  // 12 : 00 : 00 PM
+  if(meridiem == 'PM' && hour != '12') {
+    return timeMilitaryFormat = `${parseInt(hour)+12}:${minutes}:${seconds}`
+  } else if(meridiem == 'PM' && hour == '12') {
+    return timeMilitaryFormat = `12:${minutes}:${seconds}`
+  } else if(meridiem == 'AM' && hour == '12') {
+    return timeMilitaryFormat = `00:${minutes}:${seconds}`
+  } 
+  else {
+    timeMilitaryFormat = s.slice(0,8)
+    return timeMilitaryFormat
   }
 }
 
-timeConvertion('12:01:00PM')
+let result = timeConvertion('12:01:00AM')
+
+console.log(result)
